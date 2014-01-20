97e05761787cb17fe178c4f326462d84c931cb61
Joshua Marsh (icub3d) <joshua@themarshians.com>
2014-01-20 15:39:38 -0700

    Added the blocked reader.

--- wrapio.go
+++ wrapio.go
@@ -12,77 +12,62 @@
 package wrapio
 
 import (
+	"fmt"
 	"hash"
 	"io"
 	"sync"
 )
 
-// WrapFunc is the signature used for functions that wrap an io.Reader
-// or io.Writer. It is used in conjunction with some of the functions
-// below.
-type WrapFunc func([]byte) error
-
-// Wrap implements the io.Reader and io.Writer interface. It contains
-// common simple algorithms that many of the New* functions use.
+// Wrap implements the io.Closer, io.Reader, and io.Writer
+// interface. It contains common simple algorithms that many of the
+// New* functions use.
 type wrap struct {
-	// F is the function that is called during the Read/Write
-	// process. It is called after a Read() and before a Write(). An
-	// error suggests something fatal and the process should stop.
-	f WrapFunc
-	r io.Reader
-	w io.Writer
+	handler func([]byte)
+	r       io.Reader
+	w       io.Writer
 }
 
 // Read implements the io.Reader interface.
 func (w *wrap) Read(p []byte) (int, error) {
 	n, err := w.r.Read(p)
 	if n > 0 {
-		err := w.f(p[0:n])
-		if err != nil {
-			return 0, err
-		}
+		w.handler(p[:n])
 	}
 	return n, err
 }
 
 // Write implements the io.Writer interface.
 func (w *wrap) Write(p []byte) (int, error) {
-	err := w.f(p)
-	if err != nil {
-		return 0, err
-	}
+	w.handler(p)
 	return w.w.Write(p)
 }
 
 // NewFuncReader returns an io.Reader that wraps the given io.Reader
-// with the given function. Any Read() operations will run through the
-// translation function before being returned. If the function
-// encounters something fatal, an error can be returned and that will
-// be returned for the Read(). If either of the parameters are nil,
-// nil is returned.
-func NewFuncReader(f WrapFunc, r io.Reader) io.Reader {
-	if f == nil || r == nil {
+// with the given handler. Any Read() operations that read at least
+// one byte will run through the handler before being returned. If
+// either of the parameters are nil, nil is returned.
+func NewFuncReader(handler func([]byte), r io.Reader) io.Reader {
+	if handler == nil || r == nil {
 		return nil
 	}
-	return &wrap{f: func(p []byte) error {
-		return f(p)
-	}, r: r}
+	return &wrap{handler: handler, r: r}
 }
 
 // NewFuncWriter returns an io.Writer that wraps the given io.Writer
-// with the given function. Any Write() operations will run through
-// the translation function before being written. If the function
-// encounters something fatal, an error can be returned and that will
-// be returned for the Write() without data being written to the
-// original io.Writer. If either of the parameters are nil, nil is
-// returned.
-func NewFuncWriter(f WrapFunc, w io.Writer) io.Writer {
-	if f == nil || w == nil {
+// with the given handler. Any Write() operations will run through the
+// handler before being written. If either of the parameters are nil,
+// nil is returned.
+//
+// Since the handler is called with all the data before the write, if
+// an error occurs and not all of it is written, sending that data
+// again will cause it to be sent to the handler again as well. This
+// is a special case because most errors on write are fatal, but in
+// cases where writing will continue, this must be taken into account.
+func NewFuncWriter(handler func([]byte), w io.Writer) io.Writer {
+	if handler == nil || w == nil {
 		return nil
 	}
-	return &wrap{f: func(p []byte) error {
-		return f(p)
-	}, w: w}
+	return &wrap{handler: handler, w: w}
 }
 
 // NewHashReader returns an io.Reader that wraps the given io.Reader
@@ -94,9 +79,8 @@ func NewHashReader(h hash.Hash, r io.Reader) io.Reader {
 	if h == nil {
 		return nil
 	}
-	return NewFuncReader(func(p []byte) error {
+	return NewFuncReader(func(p []byte) {
 		h.Write(p)
-		return nil
 	}, r)
 }
 
@@ -109,9 +93,8 @@ func NewHashWriter(h hash.Hash, w io.Writer) io.Writer {
 	if h == nil {
 		return nil
 	}
-	return NewFuncWriter(func(p []byte) error {
+	return NewFuncWriter(func(p []byte) {
 		h.Write(p)
-		return nil
 	}, w)
 }
 
@@ -121,22 +104,23 @@ func NewHashWriter(h hash.Hash, w io.Writer) io.Writer {
 // to prevent possible race conditions.
 type Stats struct {
 	sync.Mutex
-	Total   int // The total number of bytes that have passed through.
-	Average int // The average number of bytes read or written per call.
-	Calls   int // The number of calls made to Read or Write.
+	Total   int     // The total number of bytes that have passed through.
+	Average float64 // The average number of bytes read or written per call.
+	Calls   int     // The number of calls made to Read or Write.
 }
 
-// Wrap implements the wrap interface. It analyzes the given bytes and
-// updates the statistics accordingly.
-func makeStatsWrap(s *Stats) WrapFunc {
-	return func(p []byte) error {
-		s.Lock()
-		defer s.Unlock()
-		s.Total += len(p)
-		s.Calls++
-		s.Average = s.Total / s.Calls
-		return nil
-	}
+// String implements the fmt.Stringer interface.
+func (s Stats) String() string {
+	return fmt.Sprintf("[Total: %d, Average: %f, Calls: %d]",
+		s.Total, s.Average, s.Calls)
+}
+
+func (s *Stats) update(p []byte) {
+	s.Lock()
+	defer s.Unlock()
+	s.Total += len(p)
+	s.Calls++
+	s.Average = float64(s.Total / s.Calls)
 }
 
 // NewStatsReader returns an io.Reader that wraps the given io.Reader
@@ -145,7 +129,7 @@ func makeStatsWrap(s *Stats) WrapFunc {
 // are nil, nil is returned.
 func NewStatsReader(r io.Reader) (*Stats, io.Reader) {
 	s := &Stats{}
-	return s, NewFuncReader(makeStatsWrap(s), r)
+	return s, NewFuncReader(s.update, r)
 }
 
 // NewStatsWriter returns an io.Writer that wraps the given io.Writer
@@ -154,5 +138,118 @@ func NewStatsReader(r io.Reader) (*Stats, io.Reader) {
 // are nil, nil is returned.
 func NewStatsWriter(w io.Writer) (*Stats, io.Writer) {
 	s := &Stats{}
-	return s, NewFuncWriter(makeStatsWrap(s), w)
+	return s, NewFuncWriter(s.update, w)
+}
+
+type block struct {
+	r    io.Reader
+	w    io.Writer
+	size int
+	buf  []byte
+	err  error // The non-nil error from the last Read().
+
+}
+
+// Read implements the io.Reader interface.
+func (b *block) Read(p []byte) (int, error) {
+	// If we've finished reading, we can quit.
+	if b.err != nil && len(b.buf) == 0 {
+		return 0, b.err
+	}
+	// We'll only fill p with full blocks.
+	n := (len(p) / b.size) * b.size
+	if n == 0 {
+		return 0, nil
+	}
+	if b.err == nil {
+		// Fill p temporarily and append it to our buffer.
+		l, err := b.r.Read(p)
+		b.err = err
+		b.buf = append(b.buf, p[:l]...)
+	}
+	// If the size of p if bigger than what we have, only pull the
+	// number of blocks that is in the buffer.
+	if n > len(b.buf) {
+		n = (len(b.buf) / b.size) * b.size
+	}
+	// If we've reached the end and we don't have a full block, make it
+	// our last send.
+	if b.err != nil && n == 0 {
+		n = len(b.buf)
+	}
+	// Copy what we have to p.
+	copy(p, b.buf[:n])
+	copy(b.buf, b.buf[n:])
+	b.buf = b.buf[:len(b.buf)-n]
+	return n, nil
+}
+
+// Write implements the io.Writer interface.
+func (b *block) Write(p []byte) (int, error) {
+	if b.err != nil {
+		return 0, b.err
+	}
+	// We should first append p to our buffer.
+	b.buf = append(b.buf, p...)
+	// Write out any whole blocks.
+	l := (len(b.buf) / b.size) * b.size
+	if l > 0 {
+		n, err := b.w.Write(b.buf[:l])
+		// Move the unwritten portion to the beginning of the buffer and
+		// reslice the buffer.
+		copy(b.buf, b.buf[l:])
+		b.buf = b.buf[:len(b.buf)-l]
+		// In the error case, we want to report the actual written
+		// information.
+		if err != nil {
+			b.err = err
+			return n, err
+		}
+	}
+	return len(p), nil
+}
+
+// Close implements the io.Closer interface.
+func (b *block) Close() error {
+	if b.err != nil {
+		return b.err
+	}
+	// Write out any remaining data (which wouldn't have fit into a
+	// block).
+	if len(b.buf) > 0 {
+		_, err := b.w.Write(b.buf)
+		return err
+	}
+	return nil
+}
+
+// NewBlockReader returns a reader that sends data to the given reader
+// in blocks that are a multiple of size. The one exception of this is
+// the last Read() in which there may be an incomplete block. If p in
+// Read(p) is not the length of a block, no data will be written to it
+// (i.e it will return 0, nil). This may cause an infinite loop if you
+// never give a slice larger than size.
+func NewBlockReader(size int, r io.Reader) io.Reader {
+	if r == nil || size < 1 {
+		return nil
+	}
+	return &block{r: r, size: size}
+}
+
+// NewBlockWriter returns a writer that sends data to the given writer
+// in blocks that are a multiple of size. Writes may be held if there
+// is not enough data to Write() a complete block. To adhere to the
+// io.Writer documentation though, the returned number of written
+// bytes will always be the length of the given slice unless an error
+// occurred in writing.
+//
+// Because it is impossible to tell when writing is completed, the
+// returned writer is also a closer. The close operation should be
+// called to flush out the remaining unwritten data that did not fit
+// into a block size.
+func NewBlockWriter(size int, w io.Writer) io.WriteCloser {
+	if w == nil || size < 1 {
+		return nil
+	}
+	return &block{w: w, size: size}
 }
--- wrapio_test.go
+++ wrapio_test.go
@@ -13,52 +13,11 @@ import (
 	"hash"
 	"io"
 	"io/ioutil"
+	"reflect"
 	"strings"
 	"testing"
 	"testing/iotest"
 )
-
-func TestWrap(t *testing.T) {
-	// We basically just need to test the error cases.
-	expected := fmt.Errorf("test")
-	w := wrap{
-		f: func(p []byte) error {
-			return expected
-		},
-		r: strings.NewReader("test"),
-		w: ioutil.Discard,
-	}
-	p := make([]byte, 32)
-	if n, err := w.Read(p); n != 0 || err != expected {
-		t.Errorf("Expected 0 %v with Read() wrapfunc but got: %v %v",
-			expected, n, err)
-	}
-	if n, err := w.Write(p); n != 0 || err != expected {
-		t.Errorf("Expected 0 %v with Write() wrapfunc but got: %v %v",
-			expected, n, err)
-	}
-}
-
-func Example_func() {
-	// We'll read from this using io.Copy.
-	r := strings.NewReader("This is the sample data that we are going to test with.")
-
-	// Create our wrapper which will replace spaces with |.
-	var b bytes.Buffer
-	w := NewFuncWriter(func(p []byte) error {
-		// Do the replace and then copy the change back to p.
-		n := bytes.Replace(p, []byte(" "), []byte("|"), -1)
-		copy(p, n)
-		return nil
-	}, &b)
-	io.Copy(w, r)
-
-	// Print the results.
-	fmt.Println(b.String())
-
-	// Output:
-	// This|is|the|sample|data|that|we|are|going|to|test|with.
-}
 
 func Example_hashes() {
 	// We'll read from this using io.Copy.
@@ -82,7 +41,7 @@ func Example_hashes() {
 	// 52b846d6fedeb0a90acec7ce09f7d590ec4db0e5bd1884bc74c1d81e3c00b471
 }
 
-func TestNewHashReader(t *testing.T) {
+func TestHashReader(t *testing.T) {
 	tests := []struct {
 		data     string
 		expected string
@@ -118,7 +77,7 @@ func TestNewHashReader(t *testing.T) {
 	}
 }
 
-func TestNewHashWriter(t *testing.T) {
+func TestHashWriter(t *testing.T) {
 	tests := []struct {
 		data     string
 		expected string
@@ -165,56 +124,352 @@ func Example_stats() {
 	// Print out the statistics.
 	s.Lock()
 	defer s.Unlock()
-	fmt.Println(s.Total)
-	fmt.Println(s.Calls)
-	fmt.Println(s.Average)
-
+	fmt.Println(s)
 	// Output:
-	// 55
-	// 55
-	// 1
+	// [Total: 55, Average: 1.000000, Calls: 55]
 }
 
-func TestNewStatsReader(t *testing.T) {
+func TestStatsString(t *testing.T) {
+	s := Stats{Total: 10, Average: 2.193, Calls: 5}
+	if s.String() != "[Total: 10, Average: 2.193000, Calls: 5]" {
+		t.Errorf("Stats.String() produced the wrong ouptut: %s", s)
+	}
+}
+
+func TestStatsReader(t *testing.T) {
 	tests := []struct {
 		data     string
-		expected []int
+		expected Stats
 	}{
 		{
 			data:     "this is a test.",
-			expected: []int{15, 1, 15},
+			expected: Stats{Total: 15, Average: 15, Calls: 1},
 		},
 	}
 	for k, test := range tests {
 		sr := strings.NewReader(test.data)
 		s, hr := NewStatsReader(sr)
 		ioutil.ReadAll(hr)
-		if s.Total != test.expected[0] || s.Calls != test.expected[1] ||
-			s.Average != test.expected[2] {
+		if s.Total != test.expected.Total || s.Calls != test.expected.Calls ||
+			s.Average != test.expected.Average {
 			t.Errorf("Test %v: unexpected stats, got vs expected:\n%v\n%v",
 				k, s, test.expected)
 		}
 	}
 }
 
-func TestNewStatsWriter(t *testing.T) {
+func TestStatsWriter(t *testing.T) {
 	tests := []struct {
 		data     string
-		expected []int
+		expected Stats
 	}{
 		{
 			data:     "this is a test.",
-			expected: []int{15, 1, 15},
+			expected: Stats{Total: 15, Average: 15, Calls: 1},
 		},
 	}
 	for k, test := range tests {
 		sr := strings.NewReader(test.data)
 		s, hw := NewStatsWriter(ioutil.Discard)
 		io.Copy(hw, sr)
-		if s.Total != test.expected[0] || s.Calls != test.expected[1] ||
-			s.Average != test.expected[2] {
-			t.Errorf("Test %v: unexpected stats, got vs expected:\n%v\n%v",
+		if s.Total != test.expected.Total || s.Calls != test.expected.Calls ||
+			s.Average != test.expected.Average {
+			t.Errorf("Test %v: unexpected stats, got vs expected:\n%s\n%s",
 				k, s, test.expected)
 		}
 	}
+}
+
+func Example_blocks() {
+	// This is the buffer that we'll read from.
+	buf := strings.NewReader("0123456789")
+	br := NewBlockReader(3, buf)
+	p := make([]byte, 5)
+	// Read until we get an error.
+	var err error
+	var n int
+	for err == nil {
+		n, err = br.Read(p)
+		fmt.Println(n, err, p[:n])
+	}
+	// Output:
+	// 3 <nil> [48 49 50]
+	// 3 <nil> [51 52 53]
+	// 3 <nil> [54 55 56]
+	// 1 <nil> [57]
+	// 0 EOF []
+}
+
+func TestBlockWriter(t *testing.T) {
+	// We'll test various valid sizes and lengths against strings as a
+	// sort of functional test.
+	tests := []struct {
+		size   int
+		writes []string
+		reads  []string
+		ns     []int
+		errs   []error
+		last   string
+	}{
+		{
+			size: 2,
+			writes: []string{
+				"01234", "5678", "90",
+			},
+			reads: []string{
+				"0123", "4567", "89",
+			},
+			ns: []int{
+				5, 4, 2,
+			},
+			errs: []error{
+				nil, nil, nil,
+			},
+			last: "0",
+		},
+		{
+			size: 2,
+			writes: []string{
+				"01234", "5678", "9",
+			},
+			reads: []string{
+				"0123", "4567", "89",
+			},
+			ns: []int{
+				5, 4, 1,
+			},
+			errs: []error{
+				nil, nil, nil,
+			},
+			last: "",
+		},
+	}
+	for k, test := range tests {
+		bw := new(bytes.Buffer)
+		w := NewBlockWriter(test.size, bw)
+		for x, write := range test.writes {
+			n, err := w.Write([]byte(write))
+			if n != test.ns[x] {
+				t.Errorf("Test %v(%v): n == %v, but wanted %v",
+					k, x, n, test.ns[x])
+			}
+			if err != test.errs[x] {
+				t.Errorf("Test %v(%v): err == %v, but wanted %v",
+					k, x, err, test.errs[x])
+			}
+			s := bw.String()
+			if s != test.reads[x] {
+				t.Errorf("Test %v(%v): s == '%v', but wanted '%v'",
+					k, x, s, test.reads[x])
+			}
+			bw.Reset()
+		}
+		w.Close()
+		s := bw.String()
+		if s != test.last {
+			t.Errorf("Test %v: last == '%v', but wanted '%v'",
+				k, s, test.last)
+		}
+	}
+
+	// Test a nil writer.
+	if NewBlockWriter(1, nil) != nil {
+		t.Errorf("nil io.Writer didn't return nil.")
+	}
+	if NewBlockWriter(0, &bytes.Buffer{}) != nil {
+		t.Errorf("zero size didn't return nil.")
+	}
+
+	// Test with the error writer.
+	e := ew{err: fmt.Errorf("i did it")}
+	w := NewBlockWriter(1, e)
+	for x := 0; x < 2; x++ {
+		n, err := w.Write([]byte("test"))
+		if n != 0 || err == nil {
+			t.Errorf("Test %v: bad error writer results: %v %v",
+				x, n, err)
+		}
+	}
+	err := w.Close()
+	if err == nil {
+		t.Errorf("bad error close results: %v", err)
+	}
+
+}
+
+func TestBlockReaderFunctional(t *testing.T) {
+	if NewBlockReader(0, er{}) != nil {
+		t.Errorf("zero reader size didn't return nil")
+	}
+	if NewBlockReader(1, nil) != nil {
+		t.Errorf("nil reader didn't return nil")
+	}
+	r := strings.NewReader("0123456789")
+	br := NewBlockReader(3, r)
+	s, sr := NewStatsReader(br)
+	buf := &bytes.Buffer{}
+	io.Copy(buf, sr)
+	if buf.String() != "0123456789" {
+		t.Errorf("expected output '%v' != results '%v'",
+			"0123456789", buf.String())
+	}
+	if s.Calls != 2 {
+		t.Errorf("expected calls %v != results %v",
+			2, s.Calls)
+	}
+
+}
+
+// This does some unit testing. It puts the block in an artificial
+// state and checks the expected outcome.
+func TestBlockReaderUnitTest(t *testing.T) {
+	tests := []struct {
+		p        []byte
+		expected []byte
+		block    block
+		buf      []byte
+		n        int
+		err      error
+	}{
+		// Test being at the end with no data left.
+		{
+			p:        make([]byte, 10),
+			expected: []byte{},
+			block: block{
+				err: io.EOF,
+			},
+			n:   0,
+			err: io.EOF,
+		},
+		// Test where len(p) is less than block size.
+		{
+			p:        make([]byte, 10),
+			expected: []byte{},
+			block: block{
+				size: 20,
+			},
+			n:   0,
+			err: nil,
+		},
+		// Test where we have an error, but still some in the buffer. We
+		// want to pull full blocks in this one.
+		{
+			p:        make([]byte, 10),
+			expected: []byte{48, 49, 50, 51},
+			block: block{
+				buf:  []byte("01234"),
+				size: 2,
+				err:  io.EOF,
+			},
+			buf: []byte("4"),
+			n:   4,
+			err: nil,
+		},
+		// Same as above, but p is smaller.
+		{
+			p:        make([]byte, 3),
+			expected: []byte{48, 49},
+			block: block{
+				buf:  []byte("01234"),
+				size: 2,
+				err:  io.EOF,
+			},
+			buf: []byte("234"),
+			n:   2,
+			err: nil,
+		},
+		// Same as above, but we only have a partial block.
+		{
+			p:        make([]byte, 4),
+			expected: []byte{48, 49, 50},
+			block: block{
+				buf:  []byte("012"),
+				size: 4,
+				err:  io.EOF,
+			},
+			buf: []byte{},
+			n:   3,
+			err: nil,
+		},
+		// We aren't in an error condition, so we read and append
+		{
+			p:        make([]byte, 5),
+			expected: []byte{48, 49, 50, 51},
+			block: block{
+				r: er{
+					data: []byte("34567"),
+					n:    5,
+					err:  nil,
+				},
+				buf:  []byte("012"),
+				size: 4,
+				err:  nil,
+			},
+			buf: []byte("4567"),
+			n:   4,
+			err: nil,
+		},
+		// We don't get a full block after a read
+		{
+			p:        make([]byte, 5),
+			expected: []byte{},
+			block: block{
+				r: er{
+					data: []byte(""),
+					n:    0,
+					err:  nil,
+				},
+				buf:  []byte("012"),
+				size: 4,
+				err:  nil,
+			},
+			buf: []byte("012"),
+			n:   0,
+			err: nil,
+		},
+	}
+	for k, test := range tests {
+		n, err := test.block.Read(test.p)
+		if !reflect.DeepEqual(test.p[:n], test.expected) {
+			t.Errorf("Test %v: p (%v) != expected (%v)",
+				k, test.p, test.expected)
+		}
+		if n != test.n {
+			t.Errorf("Test %v: n (%v) != expected (%v)",
+				k, n, test.n)
+		}
+		if err != test.err {
+			t.Errorf("Test %v: err (%v) != expected (%v)",
+				k, err, test.err)
+		}
+		if test.buf != nil {
+			if !reflect.DeepEqual(test.buf, test.block.buf) {
+				t.Errorf("Test %v: buf (%v) != expected (%v)",
+					k, test.block.buf, test.buf)
+			}
+		}
+	}
+}
+
+// Er is a helper for testing reads. It always writes the given data
+// to p and returns the given values.
+type er struct {
+	data []byte
+	n    int
+	err  error
+}
+
+func (e er) Read(p []byte) (int, error) {
+	copy(p, e.data)
+	return e.n, e.err
+}
+
+// Ew is a helper for testing the writers that need to error out. Any
+// call to Write() will produce the err.
+type ew struct {
+	err error
+}
+
+func (e ew) Write(p []byte) (int, error) {
+	return 0, e.err
 }
